af6f59e28daea7329355ab76688818c02d2b54b2
12boopathi <72063665+MrDevaNagalingam@users.noreply.github.com>
2025-08-20 15:24:26 +0530

    main

---
+++ FreeRTOS_Kernal/Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/portable.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/list.h
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/portable.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernal/ThirdParty/FreeRTOS/include/list.h:
