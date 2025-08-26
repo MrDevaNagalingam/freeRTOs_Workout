8afcb8dfc57d5cf3ef708b97188019766ba02e68
12boopathi <72063665+MrDevaNagalingam@users.noreply.github.com>
2025-08-26 14:41:01 +0530

    main

---
+++ FreeRTOS_Kernel_temperature/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/portable.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/task.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/list.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/timers.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/task.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/stack_macros.h
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/portable.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/task.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/list.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/timers.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/task.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_temperature/ThirdParty/FreeRTOS/include/stack_macros.h:
