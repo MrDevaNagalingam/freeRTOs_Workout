11b3e4e61513a7863c9ef1c11bc968ab4bf67c41
12boopathi <72063665+MrDevaNagalingam@users.noreply.github.com>
2025-08-29 12:59:48 +0530

    main

---
+++ FreeRTOS_Kernel_Semaphors_bt/Debug/ThirdParty/FreeRTOS/stream_buffer.d
@@ -0,0 +1,22 @@
+ThirdParty/FreeRTOS/stream_buffer.o: \
+ ../ThirdParty/FreeRTOS/stream_buffer.c \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/portable.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/task.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/list.h \
+ /home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/stream_buffer.h
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/portable.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/task.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/list.h:
+/home/boobathi/new_stm32_workspace/FreeRTOS_Kernel_Semaphors_bt/ThirdParty/FreeRTOS/include/stream_buffer.h:
